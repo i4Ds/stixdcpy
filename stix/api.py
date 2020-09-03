aafc5b5e0da3316e41b5bbca8a29822ddaa7af54
xiaohl <dr.hualinxiao@gmail.com>
2020-09-03 10:04:25 +0200

    renamed scripts

--- stix/api.py
+++ stix/api.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 """ STIX data query APIs
+
+    created on Sept. 2nd, 2020
+
 """
 
 import io
@@ -15,6 +18,16 @@ FITS_TYPES = {
 
 
 def download(url: str, desc: str):
+    """Download a file from the link and save the file to a temporary file.
+       Downloading progress will be shown in a progress bar
+
+    Args:
+        url (str): URL
+        desc (str): description to be shown on the progress bar
+
+    Returns:
+        temporary filename 
+    """    
     resp = requests.get(url, stream=True)
     f = NamedTemporaryFile(delete=False, suffix=".fits")
     chunk_size = 1024
@@ -34,7 +47,22 @@ def download(url: str, desc: str):
     return name
 
 
-def search(start_utc, stop_utc, fits_type='lc'):
+def fetch(start_utc, stop_utc, fits_type='lc'):
+    """Query data from pub023 and download FITS file from the server.
+    A fits file will be received for the packets which satistify the query condition.
+    If no data is found on pub023, a json object will be received
+    
+
+    Args:
+        start_utc (str): start UTC  in formats yyyy-dd-mmTHH:MM:SS, yyyddmmHHMMSS,  
+        stop_utc ([type]): stop UTC formats the same as above
+        fits_type (str, optional): 
+            data product type It can be 'l0', 'l1', 'l2', 'l3', 'spec', 'qlspec', 'asp', 'aspect', 'lc', 'bkg',
+    'var', 'ffl', 'cal', 'hkmin' or 'hkmax'.  Defaults to 'lc'.
+
+    Returns:
+        astropy fits object if the request is successful;  None if it is failed or no result returns
+    """    
     pattern = 'http://pub023.cs.technik.fhnw.ch/create/fits/{start_utc}/{stop_utc}/{fits_type}'
     url = pattern.format(start_utc=start_utc,
                          stop_utc=stop_utc,
