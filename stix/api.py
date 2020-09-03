e1e64b98ac77d777807815b91ab9dc8fd840cdce
xiaohl <dr.hualinxiao@gmail.com>
2020-09-03 15:31:09 +0200

    added notebooks

--- stix/api.py
+++ stix/api.py
@@ -9,6 +9,7 @@ import io
 import requests
 import numpy as np
 from tqdm import tqdm
+from datetime import datetime
 from astropy.io import fits
 from tempfile import NamedTemporaryFile
 FITS_TYPES = {
@@ -17,7 +18,7 @@ FITS_TYPES = {
 }
 
 
-def download(url: str, desc: str):
+def download(url: str, desc: str, progress_bar=True):
     """Download a file from the link and save the file to a temporary file.
        Downloading progress will be shown in a progress bar
 
@@ -28,33 +29,37 @@ def download(url: str, desc: str):
     Returns:
         temporary filename 
     """    
-    resp = requests.get(url, stream=True)
     f = NamedTemporaryFile(delete=False, suffix=".fits")
-    chunk_size = 1024
-    total = int(resp.headers.get('content-length', 0))
-    with tqdm(
+    stream=progress_bar
+    resp = requests.get(url, stream=stream)
+    if not progress_bar:
+        f.write(resp.content)
+    else:
+        chunk_size = 1024
+        total = int(resp.headers.get('content-length', 0))
+        with tqdm(
             desc=desc,
             total=total,
             unit='iB',
             unit_scale=True,
             unit_divisor=chunk_size,
-    ) as bar:
-        for data in resp.iter_content(chunk_size=chunk_size):
-            size = f.write(data)
-            bar.update(size)
+        ) as bar:
+            for data in resp.iter_content(chunk_size=chunk_size):
+                size = f.write(data)
+                bar.update(size)
     name = f.name
     f.close()
     return name
 
 
-def fetch(start_utc, stop_utc, fits_type='lc'):
+def fetch(start_utc, stop_utc, fits_type='lc', progress_bar=True):
     """Query data from pub023 and download FITS file from the server.
     A fits file will be received for the packets which satistify the query condition.
     If no data is found on pub023, a json object will be received
     
 
     Args:
-        start_utc (str): start UTC  in formats yyyy-dd-mmTHH:MM:SS, yyyddmmHHMMSS,  
+        start_utc (str): start UTC  in formats yyyy-dd-mmTHH:MM:SS, yyyddmmHHMMSS,   or datetime 
         stop_utc ([type]): stop UTC formats the same as above
         fits_type (str, optional): 
             data product type It can be 'l0', 'l1', 'l2', 'l3', 'spec', 'qlspec', 'asp', 'aspect', 'lc', 'bkg',
@@ -63,11 +68,16 @@ def fetch(start_utc, stop_utc, fits_type='lc'):
     Returns:
         astropy fits object if the request is successful;  None if it is failed or no result returns
     """    
+    if isinstance(start_utc, datetime):
+        start_utc=start_utc.strftime("%m-%d-%YT%H:%M:%S")
+    if isinstance(stop_utc, datetime):
+        stop_utc=stop_utc.strftime("%m-%d-%YT%H:%M:%S")
+
     pattern = 'http://pub023.cs.technik.fhnw.ch/create/fits/{start_utc}/{stop_utc}/{fits_type}'
     url = pattern.format(start_utc=start_utc,
                          stop_utc=stop_utc,
                          fits_type=fits_type)
-    temp_file = download(url, 'Fetching data')
+    temp_file = download(url, 'Fetching data', progress_bar)
     try:
         return fits.open(temp_file)
     except:
